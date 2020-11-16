3000ac12d54cf5c1ede74fd280b448f8a00ba818
James Jackaman <james@jackaman.net>
2020-11-16 15:01:31 -0330

    Remove legacy code from initial_conditions function

--- funcs.py
+++ funcs.py
@@ -8,7 +8,6 @@ def initial_conditions(x,t,num=1):
     if num==1:
         u = 0.05 * exp(-400*(x-0.4)**2) + 1
         h = 0.1 * exp(-400*(x-0.4)**2) + 1
-        beta = 1 - 0.04*exp(-100*(x-0.6)**2)
     else:
         raise NotImplementedError('num=%s is not an implemented initial condition' % num)
 
